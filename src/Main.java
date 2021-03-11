65c040354c4dcbf1b67727d63e0d65b4dfae87e7
Oleg Dasheev <odasheyev@mail.ru>
2021-03-11 21:39:43 +0800

    Added src/Main.java

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean registered = true;
+        float price_ruble = 8000F;
+        float mile_ruble = 20F;
+
+        float bonus = price_ruble / mile_ruble;
+        System.out.println(bonus);
+    }
+}
